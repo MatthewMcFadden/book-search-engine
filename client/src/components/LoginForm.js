29e1ad59543d342b969807ccb034eb91cf363113
Matt M <matt.mcfadden14@gmail.com>
2020-09-27 14:37:17 -0600

    login-form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/react-hooks';
 import Auth from '../utils/auth';
+import { LOGIN_USER } from '../utils/mutations';
 
 const LoginForm = () => {
   const [login, {error} ] = useMutation(LOGIN_USER);
