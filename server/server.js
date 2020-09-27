06407dc670e5561dcdeacfd35a8bb607ed802ab2
Matt M <matt.mcfadden14@gmail.com>
2020-09-27 11:26:27 -0600

    Ready to deploy

--- server/server.js
+++ server/server.js
@@ -16,6 +16,8 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
+server.applyMiddleware({ app });
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -24,8 +26,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
